d2ebe48d11eaaeb6fd5bf2fb0668bb77edaa6007
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-04-09 18:27:29 +0530

    new

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ function About(){
             <div className='about-text'>
             <h1>UPCOMING PROJECT</h1>
             <p>This is a Social Media Application 'SOUL' and This application features is little bit similar to instagram. Work in Progress </p>
-            <button>Read More</button>
+            <button>Read More..</button>
             </div>
 
             <div className='about-image'>
